29b645247845f0a04dd4b3a2f268c74b6e930e4f
WallyZhao <129118706+WallyZhao@users.noreply.github.com>
2025-03-28 20:03:13 -0400

    Update Book.java
    formatting

--- Book.java
+++ Book.java
@@ -1,49 +1,41 @@
-/*
-
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template*/
-
-//package project;//library.application for java fx package
-//make sure to change this ^
+package project;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
-/**
- *
+/* @author */
 
-@author */
 public class Book{
-  private String name;
-  private int price;
-  private final SimpleBooleanProperty available = new SimpleBooleanProperty();
-  //int[] prices = {50,100,200,500}; // all possible prices for a book (0->3)
-  //for no specified checkmark
-  public Book(String name,int price){
-    this.name = name;
-    this.price = price;
-    this.available.set(false);
-  }
-  //for specified checkmark
-  public Book(String name, int price, boolean available){
-      this.name = name;
-      this.price = price;
-      this.available.set(available);
-  }
+    private String name;
+    private int price;
+    private final SimpleBooleanProperty available = new SimpleBooleanProperty();
+    //int[] prices = {50,100,200,500}; // all possible prices for a book (0->3)
+    //for no specified checkmark
+    public Book(String name,int price){
+        this.name = name;
+        this.price = price;
+        this.available.set(false);
+    }
+    //for specified checkmark
+    public Book(String name, int price, boolean available){
+        this.name = name;
+        this.price = price;
+        this.available.set(available);
+    }
 
-  public String getName(){
-      return name;
-  }
+    public String getName(){
+        return name;
+    }
 
-  public void setName(String name){
-      this.name = name;
-  }
-  public int getPrice(){
-      return price;
-  }
-  public void setPrice(int price){
-      this.price = price;
-  }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getPrice(){
+        return price;
+    }
+    public void setPrice(int price){
+        this.price = price;
+    }
 
-  // Getter for available (modified)
+    // Getter for available (modified)
     public boolean getAvailable() {
         return available.get();
     }
@@ -53,12 +45,13 @@ public class Book{
         this.available.set(available);
     }
 
-   // Property getter to bind with the table cell
+    // Property getter to bind with the table cell
     public BooleanProperty availableProperty() {
         return available;
-    }  
-  @Override
-  public String toString(){
-      return ""+name+","+price;
-  }
+    } 
+    
+    @Override
+    public String toString(){
+        return ""+name+","+price;
+    }
 }
