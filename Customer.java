2a5f29dc9096fa9095ba18b02fc6a48c836c743b
WallyZhao <129118706+WallyZhao@users.noreply.github.com>
2025-03-28 20:02:53 -0400

    Update Customer.java
    formatting

--- Customer.java
+++ Customer.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package project;  //remember to change or something 
 
 import javafx.beans.property.BooleanProperty;
@@ -14,55 +10,54 @@ import javafx.beans.property.SimpleBooleanProperty;
 /* CUSTOMER */
 
 public class Customer extends User {
-  int points;//points used to redeem books
-  String status;
-  private final SimpleBooleanProperty available = new SimpleBooleanProperty();
+    int points;//points used to redeem books
+    String status;
+    private final SimpleBooleanProperty available = new SimpleBooleanProperty();
 
-  public Customer(String username,String password, int points){
-    this.username = username;
-    this.password = password;
-    this.points = points;
-    this.available.set(false);
-  }
-
-  //Handles Status of customer
-  //Gold  >=1000 points
-  //Silver  < 1000 points
-  public void updateStatus(){
-    if (points >= 1000){
-      status = "Gold";
-    }else{
-      status = "Silver";
+    public Customer(String username,String password, int points){
+        this.username = username;
+        this.password = password;
+        this.points = points;
+        this.available.set(false);
     }
-  }
-  public int getPoints(){
-      return points;
-  }
-  public void setPoints(int num){
-      points = num;
-  }
-  public String getStatus(){
-      return status;
-  }
-  // Getter for available (modified)
-    public boolean getAvailable() {
 
+    //Handles Status of customer
+    //Gold  >=1000 points
+    //Silver  < 1000 points
+    public void updateStatus(){
+        if (points >= 1000){
+            status = "Gold";
+        } else {
+            status = "Silver";
+        }
+    }
+    
+    public int getPoints(){
+        return points;
+    }
+    public void setPoints(int num){
+        points = num;
+    }
+    public String getStatus(){
+        return status;
+    }
+    // Getter for available (modified)
+    public boolean getAvailable() {
         return available.get();
     }
 
     // Setter for available
     public void setAvailable(boolean available) {
-
         this.available.set(available);
     }
 
-   // Property getter to bind with the table cell
+    // Property getter to bind with the table cell
     public BooleanProperty availableProperty() {
         return available;
     }  
 
-  @Override
-  public String toString(){
-      return ""+username+","+password+","+points+","+status;
-  }
+    @Override
+    public String toString(){
+        return ""+username+","+password+","+points+","+status;
+    }
 }
